9aadd3dc84e5853daaed085bfebdf057ebc06092
Jeidyo <jkolaya6@soy.sena.edu.co>
2024-05-09 10:56:49 -0500

    Agregue codigo para conectar con base de datos

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config ["SQLALCHEMY_DATABASE_URI"] - 'mysql//Proceso_textil.db'
+#app.config ["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:Mg52744319@localhost/fashionprocess'
 
 #db=SQLAlchemy(app)
 
