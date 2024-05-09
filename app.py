4ee05be1fbcce5c66af06d6ef73a045f919713fe
Jeidyo <jkolaya6@soy.sena.edu.co>
2024-05-08 19:29:10 -0500

    se trata de conectar a la base de datos> no funcional

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#app.config ["SQLALCHEMY_DATABASE_URI"] - 'mysql//Proceso_textil.db'
+
+#db=SQLAlchemy(app)
+
+#with app.app_context():
+#    db.create_all()
+
 
 from controller import *
 
 if __name__ == "__main__":
     app.run(debug=True) # si llaman a __name__ se reproduce
+
+    
