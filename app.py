8b0713436288a99a0e3398498cf78a5d8e2256fb
Jeidyo <jkolaya6@soy.sena.edu.co>
2024-05-09 12:31:27 -0500

    se comentan clases y lineas de app.py, otros en .gitignore

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config ["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:Mg52744319@localhost/fashionprocess'
+# app.config ["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:Mg52744319@localhost/fashionprocess'
 
-#db=SQLAlchemy(app)
+# db=SQLAlchemy(app)
 
-#with app.app_context():
+# with app.app_context():
 #    db.create_all()
 
 
