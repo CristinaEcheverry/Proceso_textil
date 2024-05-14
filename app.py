bffdee3666dcbe6d09cdc5850c4d57657afe90ea
Jeidyo <jkolaya6@soy.sena.edu.co>
2024-05-14 13:19:07 -0500

    Se crean nuevas carpetas para reorganisar segun MVC

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.orm import declarative_base, sessionmaker
+# from sqlalchemy import create_engine
+# # import pymysql
+
 
 app = Flask(__name__)
-# app.config ["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:Mg52744319@localhost/fashionprocess'
+# engine = create_engine("mysql+pysql://root@localhost/fashionProcess")
+
+# app.config ["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:@localhost:3306/fashionprocess'
+
+# conect = engine.connect()
+
+# Base = declarative_base()
+# Base.metadata.create_all(engine)
+# Base.metadata.bind = engine
+
+# Sesion = sessionmaker(bind=engine)
 
 # db=SQLAlchemy(app)
 
 # with app.app_context():
 #    db.create_all()
-
-
-from controller import *
+from controllers.controller import *
 
 if __name__ == "__main__":
     app.run(debug=True) # si llaman a __name__ se reproduce
+
+
 
     
