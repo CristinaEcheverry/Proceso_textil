e3b992f8eb1e0c1035b547e73e7fd81da5b31986
Jeidyo <jkolaya6@soy.sena.edu.co>
2024-06-12 13:27:31 -0500

    Commit 24: Se cambia los datos del cliente por la llave foranea clientes_id, se agrega metodos obtener_pedido_id y otros para experimentar Queda funcionando la interfas pero falla la conexion con la tabla se comenta @property porque no comprendo bien su uso

--- src/model/pedido_model.py
+++ src/model/pedido_model.py
@@ -4,36 +4,26 @@ from src.model import session, Base
 class Pedido(Base):
     __tablename__ = 'pedido'
     id = Column(Integer, primary_key=True)
-    tipo_identificacion_id = Column(SmallInteger, ForeignKey('tipo_identificacion.id'), nullable=False)
-    numero_identificacion = Column(String(15))
-    nombre = Column(String(50))
-    direccion = Column(String(50))
-    ciudad = Column(String(40))
-    telefono = Column(String(30))
-    correo = Column(String(50))
+    clientes_id = Column(Integer, ForeignKey('clientes.id'), nullable=False)
     estado_pedido_id = Column(SmallInteger, ForeignKey('estado_pedido.id'), nullable=False)
     fecha_pedido = Column(Date)
     fecha_documento = Column(DateTime)
     fecha_despacho = Column(Date)
     tipo_pedido_id = Column(SmallInteger, ForeignKey('tipo_pedido.id'), nullable=False)
-    #agrego prenda como llave foranea
+    #agrego prenda como llave foranea    
+    tipo_prenda_id = Column(SmallInteger,ForeignKey('tipo_prenda.id'), nullable=False)
     prenda_id = Column(Integer, ForeignKey('prenda.id'), nullable=False)
     material = Column(String(50), nullable=False)
     cantidad = Column(Float(10.3), nullable=False)
 
-    def __init__(self, tipo_identificacion_id, numero_identificacion, nombre, direccion, ciudad, telefono, correo, estado_pedido_id, fecha_pedido, fecha_documento, fecha_despacho, tipo_pedido_id, prenda_id, material,cantidad):
-        self.tipo_identificacion = tipo_identificacion_id
-        self.numero_identificacion = numero_identificacion
-        self.nombre = nombre
-        self.direccion = direccion
-        self.ciudad = ciudad
-        self.telefono = telefono
-        self.correo = correo
-        self.estado_pedido = estado_pedido_id
+    def __init__(self, clientes_id, estado_pedido_id, fecha_pedido, fecha_documento, fecha_despacho, tipo_pedido_id, tipo_prenda_id, prenda_id, material,cantidad):
+        self.clientes_id = clientes_id
+        self.estado_pedido_id = estado_pedido_id
         self.fecha_pedido = fecha_pedido
         self.fecha_documento = fecha_documento
         self.fecha_despacho = fecha_despacho
-        self.tipo_pedido = tipo_pedido_id
+        self.tipo_pedido_id = tipo_pedido_id
+        self.tipo_prenda_id = tipo_prenda_id
         self.prenda_id = prenda_id
         self.material = material
         self.cantidad = cantidad
@@ -42,8 +32,13 @@ class Pedido(Base):
         session.add(self)
         session.commit()
 
-    def optener_pedido(self):
+    @staticmethod
+    def obtener_pedido():
         return session.query(Pedido).all()
+    
+    @staticmethod
+    def obtener_pedido_id(id):
+        return session.query(Pedido).filter_by(id=id).first()
 
     def modificar_pedido(self):
         session.commit()
@@ -51,6 +46,10 @@ class Pedido(Base):
     def eliminar_pedido(self):
         session.delete(self)
         session.commit()
+
+    # @property
+    # def tipo_prenda_descripcion(self):
+    #     return self.prenda.tipo_prenda.descripcion
     
 
 
