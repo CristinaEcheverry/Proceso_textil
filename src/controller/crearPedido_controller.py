8303659ae533cb88eea07050a3f7dc7f4eb43371
Jeidyo <jkolaya6@soy.sena.edu.co>
2024-06-12 13:32:01 -0500

    Commit 25: Se cambia los datos del cliente por clientes_id

--- src/controller/crearPedido_controller.py
+++ src/controller/crearPedido_controller.py
@@ -2,33 +2,31 @@ from src.app import app
 from flask import render_template, request, redirect, url_for
 from flask_controller import FlaskController
 from src.model.pedido_model import Pedido
-from src.model.tipoPrenda_model import TipoPrenda
 from src.model.prenda_model import Prenda
-
+from src.model.enums.enum_tipoIden import TipoIdentificacionEnum
+from src.model.enums.enum_estadoPedido import EstadoPedidoEnum
+from src.model.enums.enum_tipoPedido import TipoPedidoEnum
+from src.model.enums.enum_tipoPrenda import TipoPrendaEnum
 
 class ProductosController(FlaskController):
     @app.route("/crearPedido", methods=['GET','POST'])
     def crearPedido():
         if request.method == 'POST':
-            tipo_identificacion = request.form.get('tipo_identificacion')
-            numero_identificacion = request.form.get('num_identificacion')
-            nombre = request.form.get('nombre')
-            direccion = request.form.get('direccion')
-            ciudad = request.form.get('ciudad')
-            telefono = request.form.get('telefono')
-            correo = request.form.get('correo')
-            estado_pedido = request.form.get('estado-pedido')
-            fecha_pedido = request.form.get('fecha-pedido')
+            clientes_id = request.form.get('clientes')
+            estado_pedido_id = request.form.get('estados')
+            fecha_pedido = request.form.get('fecha-pedido')        
             fecha_documento = request.form.get('fecha-recibido')
             fecha_despacho = request.form.get('fecha-despacho')
-            tipo_pedido = request.form.get('tipo-pedido')
-            tipo_prenda_id = request.form.get('tipo_prenda_id')
-            prenda_id = request.form.get('prenda_id')
+            tipo_pedido_id = request.form.get('tipos')
+            tipo_prenda_id = str(request.form.get('tipo_prenda'))
+            prenda_id = request.form.get('prenda')
             material = request.form.get('material')
             cantidad = request.form.get('cantidad')
-            pedido = Pedido(nombre, direccion, tipo_identificacion, numero_identificacion, telefono, correo, ciudad, estado_pedido, fecha_pedido, fecha_documento, fecha_despacho, tipo_pedido, tipo_prenda_id, prenda_id, material,cantidad)
-            Pedido.agregar_pedido(pedido)
+            nuevo_pedido = Pedido(clientes_id, estado_pedido_id, fecha_pedido, fecha_documento, fecha_despacho, tipo_pedido_id, tipo_prenda_id, prenda_id, material, cantidad)
+            Pedido.agregar_pedido(nuevo_pedido) 
+             
             return redirect(url_for('crearPedido'))
-        tipo_prenda_id = TipoPrenda.obtener_tipo_prendas()
-        prenda_id = Prenda.obtener_prendas()
-        return render_template("crearPedido_form.html", title= "Crear pedido")
+        prendas = Prenda.mostrar_prenda()
+        pedido = Pedido.optener_pedido()
+        return render_template('crearPedido_form.html', enum_idValue=TipoIdentificacionEnum, enum_estadoPeValue=EstadoPedidoEnum, enum_tipoPeValue=TipoPedidoEnum, enum_values=TipoPrendaEnum, prendas=prendas, pedidos=pedido)
+            
