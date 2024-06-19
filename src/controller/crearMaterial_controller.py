a016950f91e263834f580e2a6910b2a1323b11a1
Jeidyo <jkolaya6@soy.sena.edu.co>
2024-06-18 20:20:18 -0500

    Commit 34: importo flaskController y creo una clase que lo hereda, importo Api de flask_restful para crear una ruta de conexión a API

--- src/controller/crearMaterial_controller.py
+++ src/controller/crearMaterial_controller.py
@@ -1,22 +1,33 @@
 from src.app import app
+from flask_controller import FlaskController 
 from flask import render_template, request, redirect, url_for 
 from src.model.material_model import Material
 from src.model.enums.enum_estadoMaterial import EstadoMaterialEnum
+from flask_restful import Api
 
-@app.route("/ingresoMaterial", methods=["GET", "POST"])
-def ingresoMaterial():
-    if request.method == "POST":
-        codigo = request.form.get("codigo_material")
-        num_lote = request.form.get("num_lote")
-        producto = request.form.get("producto")
-        cantidad = request.form.get("amount")
-        proveedor = request.form.get("proveedor")
-        color = request.form.get("color")
-        estado_material = request.form.get("estado_material")
-        descripcion = request.form.get("descripcion")
-        fecha_documento = request.form.get("fecha-recibido")
-        nuevo_material = Material(codigo, num_lote, producto, cantidad, proveedor, color, estado_material, descripcion, fecha_documento)
-        Material.agregar_material(nuevo_material)                     
-        return redirect(url_for("ingresoMaterial"))
-    material = Material.obtener_material()
-    return render_template("ingresoMaterial_form.html", material=material, enum_estadoMaterial=EstadoMaterialEnum)
+#importo mi api
+from src.apis.crearMaterial_api import CrearMaterialApi
+
+class CrearMaterial(FlaskController):
+    api = Api(app)
+
+    # aqui creo una ruta para hacer el llamado a mi api
+    api.add_resource(CrearMaterialApi, "/api/crearMaterial") 
+
+    @app.route("/ingresoMaterial", methods=["GET", "POST"])
+    def ingresoMaterial():
+        if request.method == "POST":
+            codigo = request.form.get("codigo_material")
+            num_lote = request.form.get("num_lote")
+            producto = request.form.get("producto")
+            cantidad = request.form.get("amount")
+            proveedor = request.form.get("proveedor")
+            color = request.form.get("color")
+            estado_material = request.form.get("estado_material")
+            descripcion = request.form.get("descripcion")
+            fecha_documento = request.form.get("fecha-recibido")
+            nuevo_material = Material(codigo, num_lote, producto, cantidad, proveedor, color, estado_material, descripcion, fecha_documento)
+            Material.agregar_material(nuevo_material)                     
+            return redirect(url_for("ingresoMaterial"))
+        material = Material.obtener_material()
+        return render_template("ingresoMaterial_form.html", material=material, enum_estadoMaterial=EstadoMaterialEnum)
