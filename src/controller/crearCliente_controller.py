6b0cb90b47e6a0c0b064ea654e8f431032e9e507
Jeidyo <jkolaya6@soy.sena.edu.co>
2024-07-04 23:35:44 -0500

    Commit 38: Importo flaskControler y creo clase clientesController donde agrego todas las rutas

--- src/controller/crearCliente_controller.py
+++ src/controller/crearCliente_controller.py
@@ -3,25 +3,27 @@ from flask import render_template, request, redirect, url_for
 from src.model.clientes_model import Clientes
 from src.model.tipoIdent_model import TipoIdentificacion
 from src.model.tipoPersona_model import TipoPersona
+from flask_controller import FlaskController
 
-@app.route('/crearCliente', methods=['GET','POST'])
-def crearCliente():
-    if request.method == 'POST':
-        tipo_identificacion_id = request.form.get('tipo_identificacion')
-        numero_identificacion = request.form.get('num_identificacion')
-        tipo_persona = request.form.get('tipo_persona')
-        nombre = request.form.get('nombre')
-        direccion = request.form.get('direccion')
-        ciudad = request.form.get('ciudad')
-        telefono = request.form.get('telefono')
-        correo = request.form.get('correo')
-        cliente_nuevo = Clientes(tipo_identificacion_id, numero_identificacion, tipo_persona, nombre, direccion, ciudad, telefono, correo)
-        Clientes.agregar_cliente(cliente_nuevo)
-        return redirect(url_for('crearCliente'))
-    identificar = TipoIdentificacion.obtener_tipo_identificacion()
-    tipoPersona = TipoPersona.obtener_tipo_persona()
-    clientes = Clientes.obtener_cliente()
-    return render_template('crearCliente.html', title= 'CrearCliente', clientes=clientes, identificacion=identificar, tipoPersona=tipoPersona)
+class ClientesController(FlaskController):
+    @app.route('/crearCliente', methods=['GET','POST'])
+    def crearCliente():
+        if request.method == 'POST':
+            tipo_identificacion_id = request.form.get('tipo_identificacion')
+            numero_identificacion = request.form.get('num_identificacion')
+            tipo_persona = request.form.get('tipo_persona')
+            nombre = request.form.get('nombre')
+            direccion = request.form.get('direccion')
+            ciudad = request.form.get('ciudad')
+            telefono = request.form.get('telefono')
+            correo = request.form.get('correo')
+            cliente_nuevo = Clientes(tipo_identificacion_id, numero_identificacion, tipo_persona, nombre, direccion, ciudad, telefono, correo)
+            Clientes.agregar_cliente(cliente_nuevo)
+            return redirect(url_for('crearCliente'))
+        identificar = TipoIdentificacion.obtener_tipo_identificacion()
+        tipoPersona = TipoPersona.obtener_tipo_persona()
+        clientes = Clientes.obtener_cliente()
+        return render_template('crearCliente.html', title= 'CrearCliente', clientes=clientes, identificacion=identificar, tipoPersona=tipoPersona)
 
 
 #manejo de errores
