7ef0d90199d08d86f667163cdc410f86b766a376
Jeidyo <jkolaya6@soy.sena.edu.co>
2024-07-19 21:49:08 -0500

    Commit 44: Separo el detalle de pedido en una clase, creo un método estático para poder obtener el último número del pedido para que no se repita

--- src/model/pedido_model.py
+++ src/model/pedido_model.py
@@ -1,32 +1,25 @@
-from sqlalchemy import Column, Float, SmallInteger, Integer, String, Enum, Date, DateTime, ForeignKey
+from sqlalchemy import Column, Float, SmallInteger, Integer, String, Enum, Date, DateTime, ForeignKey, func
 from src.model import session, Base
 
 class Pedido(Base):
     __tablename__ = 'pedido'
     id = Column(Integer, primary_key=True)
+    numero_pedido = Column(String(10), nullable=False)
     clientes_id = Column(Integer, ForeignKey('clientes.id'), nullable=False)
     estado_pedido_id = Column(SmallInteger, ForeignKey('estado_pedido.id'), nullable=False)
     fecha_pedido = Column(Date)
     fecha_documento = Column(DateTime)
     fecha_despacho = Column(Date)
     tipo_pedido_id = Column(SmallInteger, ForeignKey('tipo_pedido.id'), nullable=False)
-    #agrego prenda como llave foranea    
-    tipo_prenda_id = Column(SmallInteger,ForeignKey('tipo_prenda.id'), nullable=False)
-    prenda_id = Column(Integer, ForeignKey('prenda.id'), nullable=False)
-    material = Column(String(50), nullable=False)
-    cantidad = Column(Float(10.3), nullable=False)
 
-    def __init__(self, clientes_id, estado_pedido_id, fecha_pedido, fecha_documento, fecha_despacho, tipo_pedido_id, tipo_prenda_id, prenda_id, material,cantidad):
+    def __init__(self, numero_pedido, clientes_id, estado_pedido_id, fecha_pedido, fecha_documento, fecha_despacho, tipo_pedido_id):    # tipo_prenda_id, prenda_id, material,cantidad
+        self.numero_pedido = numero_pedido
         self.clientes_id = clientes_id
         self.estado_pedido_id = estado_pedido_id
         self.fecha_pedido = fecha_pedido
         self.fecha_documento = fecha_documento
         self.fecha_despacho = fecha_despacho
         self.tipo_pedido_id = tipo_pedido_id
-        self.tipo_prenda_id = tipo_prenda_id
-        self.prenda_id = prenda_id
-        self.material = material
-        self.cantidad = cantidad
 
     def agregar_pedido(self):
         session.add(self)
@@ -47,10 +40,28 @@ class Pedido(Base):
         session.delete(self)
         session.commit()
 
-    # @property
-    # def tipo_prenda_descripcion(self):
-    #     return self.prenda.tipo_prenda.descripcion
-    
+    @staticmethod
+    def obtener_ultimo_id():
+        ultimo_id = session.query(func.max(Pedido.id)).scalar()
+        return ultimo_id if ultimo_id is not None else 0
 
-
     
+class PedidoDetalle(Base):
+    __tablename__ = 'pedido_detalle'
+    id = Column(Integer, primary_key=True)
+    pedido_id = Column(Integer, ForeignKey('pedido.id'), nullable=False)
+    tipo_prenda_id = Column(SmallInteger, ForeignKey('tipo_prenda.id'), nullable=False)
+    prenda_id = Column(Integer, ForeignKey('prenda.id'), nullable=False)
+    material = Column(String(50), nullable=False)
+    cantidad = Column(Float(10.3), nullable=False)
+
+    def __init__(self, pedido_id, tipo_prenda_id, prenda_id, material, cantidad):
+        self.pedido_id = pedido_id
+        self.tipo_prenda_id = tipo_prenda_id
+        self.prenda_id = prenda_id
+        self.material = material
+        self.cantidad = cantidad
+
+    def agregar_detalle(self):
+        session.add(self)
+        session.commit()
