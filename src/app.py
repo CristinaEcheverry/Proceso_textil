f6585ea5c586080f6fb2d542e369da755cdb8c5c
Jeidyo <jkolaya6@soy.sena.edu.co>
2024-07-22 20:47:10 -0500

    Commit 55: Importo flask_cors para hacer peticiones desde el front en las APIs y agrego una configuración para que acepte todas las peticiones, algo que no se debe hacer por seguridad. También agrego una llave de sesion porque el profesor asi lo tenía y quizás lo usare más adelante

--- src/app.py
+++ src/app.py
@@ -2,7 +2,19 @@ from flask import Flask
 from src.model import Base, engine
 from flask_controller import FlaskControllerRegister
 
+#se importa el cors para poder hacer peticiones desde el front
+from flask_cors import CORS
+
 app = Flask(__name__)
+
+#se agrega el cors a la app y se configura para que solo acepte peticiones con el header Content-Type
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+#se crea la llave para la sesion
+app.secret_key = 'llave_para_sesion'
+app.debug = True
+
 register = FlaskControllerRegister(app)
 register.register_package("src.controller")
 
