b57268cce775d62f36ffcd0e53ad7fb099dbda0b
Jeidy Olaya <jkolaya6@soy.sena.edu.co>
2024-08-27 17:20:59 -0500

    Commit 58: modifico el controlador para que funcione el boton consultar pedido y agrego ruta para eliminar e pedido, funciones que traen la información uniendo tablas, la plantilla se adecua para que traiga la información que se pide en el controlador. Queda funcionando

--- src/controller/consultarPedido_controller.py
+++ src/controller/consultarPedido_controller.py
@@ -1,7 +1,58 @@
 from src.app import app
 # from model import * -> Aquí se tiene que importar todos los modelos o clases
-from flask import render_template  # render_template es para usar las vistas
+from flask import render_template, request, redirect, url_for, flash  # render_template es para usar las vistas
+from flask_controller import FlaskController
+from src.model.pedido_model import Pedido, PedidoDetalle
+from src.model.clientes_model import Clientes
+from src.model.estadoPedido_model import EstadoPedido
+from src.model.tipoIdent_model import TipoIdentificacion
+from src.model.estadoPedido_model import EstadoPedido
 
-@app.route("/consultarPedido")
-def consultarPedido():
-    return render_template("consultarPedido_form.html", title= "Consultar pedido")
+
+class ConsultarPedidoController(FlaskController):
+    @app.route("/consultarPedido", methods=["GET", "POST"])
+    def consultarPedido():
+        '''Aquí va el código para consultar los pedidos, se debe obtener los pedidos y 
+        sus detalles para mostrarlos en la vista.'''
+        pedidos = []
+        if request.method == 'POST':
+            tipo_identificacion = request.form.get('tipo_identificacion')
+            num_identificacion = request.form.get('num_identificacion')
+            nombre = request.form.get('nombre')
+            num_pedido = request.form.get('num_pedido')
+            estado_pedido_id = request.form.get('estados')
+            fecha_pedido = request.form.get('fecha-pedido')
+            fecha_despacho = request.form.get('fecha-despacho')
+            
+            print(f"tipo_identificacion: {tipo_identificacion}")
+            print(f"num_identificacion: {num_identificacion}")
+            print(f"nombre: {nombre}")
+            print(f"estado_pedido_id: {estado_pedido_id}")
+            print(f"num_pedido: {num_pedido}")
+            print(f"fecha_pedido: {fecha_pedido}")
+            print(f"fecha_despacho: {fecha_despacho}")
+
+            pedidos = PedidoDetalle.obtener_detalle_por_criterio(
+                tipo_identificacion=tipo_identificacion,
+                num_identificacion=num_identificacion,
+                nombre=nombre,
+                estado_pedido_id=estado_pedido_id,
+                num_pedido=num_pedido,
+                fecha_pedido=fecha_pedido,
+                fecha_despacho=fecha_despacho
+            )
+
+        tipoID = TipoIdentificacion.obtener_tipo_identificacion()
+        estadoPedido = EstadoPedido.obtener_estado_pedido()
+        return render_template("consultarPedido_form.html", pedidos=pedidos, tipoID=tipoID, estadoPedido=estadoPedido)
+        
+    #ruta para eliminar un pedido por medio de la consulta
+    @app.route('/eliminarPedido/<int:id>', methods=['GET'])
+    def eliminarPedido(id):
+        '''Aquí va el código para eliminar un pedido.'''
+        pedido = Pedido.obtener_pedido_id(id)
+        detalle = PedidoDetalle.obtener_detalle(pedido_id=id)
+        pedido_eliminado = Pedido.eliminar_pedido(pedido)
+        detalle_eliminado = PedidoDetalle.eliminar_pedido_detalle(detalle)  
+        flash("Pedido eliminado correctamente.")      
+        return redirect(url_for('consultarPedido'))
--- src/model/pedido_model.py
+++ src/model/pedido_model.py
@@ -1,5 +1,9 @@
-from sqlalchemy import Column, Float, SmallInteger, Integer, String, Enum, Date, DateTime, ForeignKey, func
+from sqlalchemy import Column, Float, SmallInteger, Integer, String, Enum, Date, DateTime, ForeignKey, func, or_
 from src.model import session, Base
+from sqlalchemy.orm import relationship, joinedload
+from src.model.clientes_model import Clientes
+from src.model.tipoIdent_model import TipoIdentificacion
+from src.model.estadoPedido_model import EstadoPedido
 
 class Pedido(Base):
     __tablename__ = 'pedido'
@@ -12,7 +16,13 @@ class Pedido(Base):
     fecha_despacho = Column(Date)
     tipo_pedido_id = Column(SmallInteger, ForeignKey('tipo_pedido.id'), nullable=False)
 
-    def __init__(self, numero_pedido, clientes_id, estado_pedido_id, fecha_pedido, fecha_documento, fecha_despacho, tipo_pedido_id):    # tipo_prenda_id, prenda_id, material,cantidad
+#relacion con tabla clientes
+    clientes = relationship('Clientes', backref='pedido')
+    #relacion con tabla Detalle_pedido
+    detalle_pedido = relationship('PedidoDetalle', backref='pedido')
+
+    def __init__(self, numero_pedido, clientes_id, estado_pedido_id, 
+    fecha_pedido, fecha_documento, fecha_despacho, tipo_pedido_id):    # tipo_prenda_id, prenda_id, material,cantidad
         self.numero_pedido = numero_pedido
         self.clientes_id = clientes_id
         self.estado_pedido_id = estado_pedido_id
@@ -20,6 +30,19 @@ class Pedido(Base):
         self.fecha_documento = fecha_documento
         self.fecha_despacho = fecha_despacho
         self.tipo_pedido_id = tipo_pedido_id
+
+    #serializar los datos de pedido
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "numero_pedido": self.numero_pedido,
+            "clientes_id": self.clientes_id,
+            "estado_pedido_id": self.estado_pedido_id,
+            "fecha_pedido": self.fecha_pedido,
+            "fecha_documento": self.fecha_documento,
+            "fecha_despacho": self.fecha_despacho,
+            "tipo_pedido_id": self.tipo_pedido_id
+        }
 
     def agregar_pedido(self):
         session.add(self)
@@ -34,9 +57,11 @@ class Pedido(Base):
         return session.query(Pedido).filter_by(id=id).first()
 
     def modificar_pedido(self):
+        session.update(self)
         session.commit()
 
     def eliminar_pedido(self):
+        '''Función que elimina el pedido.'''
         session.delete(self)
         session.commit()
 
@@ -49,19 +74,83 @@ class Pedido(Base):
 class PedidoDetalle(Base):
     __tablename__ = 'pedido_detalle'
     id = Column(Integer, primary_key=True)
-    numero_pedido = Column(String(10), nullable=False)
+    pedido_id = Column(Integer, ForeignKey('pedido.id'), nullable=False)
     tipo_prenda_id = Column(SmallInteger, ForeignKey('tipo_prenda.id'), nullable=False)
     prenda_id = Column(Integer, ForeignKey('prenda.id'), nullable=False)
     material = Column(String(50), nullable=False)
     cantidad = Column(Float(10.3), nullable=False)
 
-    def __init__(self, numero_pedido, tipo_prenda_id, prenda_id, material, cantidad):
-        self.numero_pedido = numero_pedido
+    def __init__(self, pedido_id, tipo_prenda_id, prenda_id, material, cantidad):
+        self.pedido_id = pedido_id
         self.tipo_prenda_id = tipo_prenda_id
         self.prenda_id = prenda_id
         self.material = material
         self.cantidad = cantidad
 
+    #serializar los datos de pedido_detalle
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "pedido_id": self.pedido_id,
+            "tipo_prenda_id": self.tipo_prenda_id,
+            "prenda_id": self.prenda_id,
+            "material": self.material,
+            "cantidad": self.cantidad
+        }
+
     def agregar_detalle(self):
         session.add(self)
         session.commit()
+
+    def obtener_pedido_detalle():
+        return session.query(PedidoDetalle).all()
+
+    @staticmethod
+    def obtener_detalle(pedido_id):
+        return session.query(PedidoDetalle).filter_by(pedido_id=pedido_id).all()
+
+    #una función que recibe cualquier dato(numero de pedido y/o numero identificación y/o fechas, etc) y lo busca en la tabla pedido_detalle
+    @staticmethod
+    def obtener_detalle_por_criterio(tipo_identificacion=None, num_identificacion=None, nombre=None, estado_pedido_id=None, num_pedido=None, fecha_pedido=None, fecha_despacho=None):
+        query = session.query(Pedido).join(Pedido.clientes).join(Pedido.estado_pedido)
+    
+        # Incluye las relaciones en la carga de la consulta
+        query = query.options(joinedload(Pedido.clientes).joinedload(Clientes.tipo_identificacion), joinedload(Pedido.estado_pedido))
+        if tipo_identificacion:
+            query = query.filter(TipoIdentificacion.codigo == tipo_identificacion)
+        if num_identificacion:
+            query = query.filter(Clientes.numero_identificacion == num_identificacion)
+        if nombre:
+            query = query.filter(Clientes.nombre == nombre)
+        if estado_pedido_id:
+            query = query.filter(EstadoPedido.estados == estado_pedido_id)
+        if num_pedido:
+            query = query.filter(Pedido.numero_pedido == num_pedido)
+        if fecha_pedido:
+            query = query.filter(Pedido.fecha_pedido == fecha_pedido)
+        if fecha_despacho:
+            query = query.filter(Pedido.fecha_despacho == fecha_despacho)
+
+        return query.all()
+
+    def modificar_detalle(self):
+        '''Función que modifica el detalle del pedido.'''
+        for pedido in self:
+            session.update(pedido)
+        session.commit()
+
+    def eliminar_pedido_detalle(self):
+        '''Función que elimina el detalle pedido y el pedido.'''
+        for pedido in self:
+            session.delete(pedido)
+        session.commit()
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "pedido_id": self.pedido_id,
+            "tipo_prenda_id": self.tipo_prenda_id,
+            "prenda_id": self.prenda_id,
+            "material": self.material,
+            "cantidad": self.cantidad
+        }   
