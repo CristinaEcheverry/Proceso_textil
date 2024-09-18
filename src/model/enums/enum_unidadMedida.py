9b194c762888ba64a048142fa696043223bf8f40
Jeidyo <jkolaya6@soy.sena.edu.co>
2024-09-18 15:23:43 -0500

    Commit 87: Creo enums para las unidades de medida que se utilizaran al ingresar el material

---
+++ src/model/enums/enum_unidadMedida.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class UnidadMedidaEnum(Enum):
+    mtr = 'metros'
+    cm = 'centimetros'
+    mm = 'milimetros'
+    kg = 'kilogramos'
+    g = 'gramos'
+    l = 'litros'
+    ml = 'mililitros'
+    und = 'unidad'
+    paquete = 'paquete'
+    caja = 'caja'
+    rollo = 'rollo'
+    millar = 'millar'
+    docena = 'docena'
+    par = 'par'
+    
